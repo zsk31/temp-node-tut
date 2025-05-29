c99c545d3893360e584e9237492d599286300388
sjzestfulboss <82993441+sjzestfulboss@users.noreply.github.com>
2025-05-28 17:20:52 -0700

    Updated app.js and package files after writing event loop code

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-// npm - global command, comes with Node.js
-// npm --version
+const { readFile } = require('fs')
 
-//local dependency - use it only in this particular project
-// npm i <packageName>
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+ if (err) {
+  console.log(err)
+  return
+ }
+ console.log(result)
+ console.log('completed first task')
 
-//global dependency - use it in any project
-// npm install -g <packageName>
-// sudo instal -g <packageName> (mac)
-
-//package.json - manifest file (stores important info about project/package)
-//manual approach (create package.json in the root, create properties etc.)
-//npm init (step by step, press enter to skip)
-//npm init -y (everything default)
-
-const _ = require('lodash')
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+})
+console.log('starting next task')
